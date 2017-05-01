cc5adf7fc3693c7116257537ad62d5e9f90d15ad
Nam Ngo(nango) <nango@microsoft.com>
2017-04-30 22:32:37 -0700

    foo bar

--- src/Attiny85/Attiny85/main.c
+++ src/Attiny85/Attiny85/main.c
@@ -10,76 +10,15 @@
 #include <avr/sleep.h>    // Sleep Modes
 #include <avr/power.h>    // Power management
 #include <avr/wdt.h>      // Watchdog timer
-
-#define _BV(BIT)   (1<<BIT)
-
-//****************************************************************
-// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
-// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
-void setup_watchdog(int ii) {
-    //byte bb;
-    //int ww;
-    //if (ii > 9 ) ii=9;
-    //bb=ii & 7;
-    //if (ii > 7) bb|= (1<<5);
-    //bb|= (1<<WDCE);
-    //ww=bb;
-    //MCUSR &= ~(1<<WDRF);
-    //// start timed sequence
-    //watchdogRegister |= (1<<WDCE) | (1<<WDE);
-    //// set new watchdog timeout value
-    //watchdogRegister = bb;
-    //watchdogRegister |= _BV(WDIE);
-}
-
-void initADC()
-{
-  /* this function initialises the ADC 
-
-        ADC Prescaler Notes:
-	--------------------
-
-	   ADC Prescaler needs to be set so that the ADC input frequency is between 50 - 200kHz.
-  
-           For more information, see table 17.5 "ADC Prescaler Selections" in 
-           chapter 17.13.2 "ADCSRA – ADC Control and Status Register A"
-          (pages 140 and 141 on the complete ATtiny25/45/85 datasheet, Rev. 2586M–AVR–07/10)
-
-           Valid prescaler values for various clock speeds
-	
-	     Clock   Available prescaler values
-           ---------------------------------------
-             1 MHz   8 (125kHz), 16 (62.5kHz)
-             4 MHz   32 (125kHz), 64 (62.5kHz)
-             8 MHz   64 (125kHz), 128 (62.5kHz)
-            16 MHz   128 (125kHz)
-
-           Below example set prescaler to 128 for mcu running at 8MHz
-           (check the datasheet for the proper bit values to set the prescaler)
-  */
-
-  // 8-bit resolution
-  // set ADLAR to 1 to enable the Left-shift result (only bits ADC9..ADC2 are available)
-  // then, only reading ADCH is sufficient for 8-bit results (256 values)
-
-  ADCSRA = 
-            (1 << ADPS2) |     // set prescaler to 128, bit 2
-            (1 << ADPS1) |     // set prescaler to 128, bit 1
-            (1 << ADPS0);      // set prescaler to 128, bit 0
-}
-
-void initWatchDog()
-{
-    if(MCUSR & _BV(WDRF)) // If a reset was caused by the Watchdog Timer...
-    {
-        MCUSR &= ~_BV(WDRF);                 // Clear the WDT reset flag
-        WDTCR |= (1<<WDCE) | (0<<WDE);   // Enable the WD Change Bit
-        WDTCR = 0x00;                      // Disable the WDT
-    }    
-}
+#include <avr/interrupt.h>      // Watchdog timer
 
 uint16_t read_analog(int ch)
 {
+     ADCSRA =
+     (1 << ADPS2) |     // set prescaler to 128, bit 2
+     (1 << ADPS1) |     // set prescaler to 128, bit 1
+     (1 << ADPS0);      // set prescaler to 128, bit 0
+     
     ADCSRA |= (1 << ADEN); // turn on adc
     
     ch &= 0b00000111;  // AND operation with 7
@@ -92,12 +31,12 @@ uint16_t read_analog(int ch)
     // wait for conversion to complete
     // ADSC becomes ’0? again
     // till then, run loop continuously
-    while(ADCSRA & (1<<ADSC));
+    while (ADCSRA & (1<<ADSC));
     
     uint16_t adc = ADC;
     
     // turn off adc
-    ADCSRA &= ~(1 << ADEN); // turn on adc
+    ADCSRA &= ~(1 << ADEN); // turn off adc
     
     return (adc);
     
@@ -105,29 +44,91 @@ uint16_t read_analog(int ch)
 
 #define LED_PORT PB1
 #define LDR_PORT PB3
+#define PIR_PORT PB0
+#define LightDarkEnought 650
+#define Light_Time_In_Second 120
+
+// this variable represent the state of the led
+// 0 - led is off
+// > 0 the number of second the led has been on
+volatile int led_state = 0; 
+
+void check_light()
+{
+    cli();
+    if (led_state == 0)
+    {
+        uint16_t light_value = read_analog(LDR_PORT);
+        if (light_value < LightDarkEnought)
+        {
+            int has_movement = PINB & (1 << PIR_PORT);
+            if (has_movement)
+            {
+                led_state = 1;
+                // turn on the led
+                PORTB |= (1 << LED_PORT);
+            }
+        }
+    }
+    else
+    {
+        led_state ++;
+        if (led_state > Light_Time_In_Second)
+        {
+            led_state = 0;
+            // turn off the led
+            PORTB &= ~(1 << LED_PORT);
+        }
+    }
+    
+    sei();
+    
+}
+
+void resetWatchdog ()
+{
+    // clear various "reset" flags
+    MCUSR = 0;
+    // allow changes, disable reset, clear existing interrupt
+    WDTCR = (1 << WDCE) | (1 << WDE) | (1 << WDIF);
+    // set interrupt mode and an interval (WDE must be changed from 1 to 0 here)
+    WDTCR = (1 << WDIE) | (1 << WDP3) | (1 << WDP0);    // set WDIE, and 8 seconds delay
+    // pat the dog
+    wdt_reset();
+}  // end of resetWatchdog
+
+void setup()
+{
+    cli();
+    resetWatchdog();
+    DDRB |= (1 << LED_PORT);
+    sei();
+}
+
+void go_to_sleep()
+{
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    power_all_disable();
+    resetWatchdog();
+    sleep_enable();
+    sei(); 
+    sleep_cpu();
+    sleep_disable();
+    power_all_enable();
+}
 
 int main(void)
 {
-    DDRB |= (1 << LED_PORT);
-    
-    initADC();
+    setup();
 
     while(1)
     {
-        uint16_t raw_adc = read_analog(LDR_PORT);   // add lobyte and hibyte
-
-        if (raw_adc > 512)
-        {
-            // ADC input voltage is more than half of the internal 1.1V reference voltage
-            PORTB |= (1 << LED_PORT);
-            PORTB &= ~(1 << LED_PORT);
-        } 
-        else 
-        {
-             PORTB &= ~(1 << LED_PORT);
-
-        }
-
+        check_light();
+        go_to_sleep();
     }
 }
 
+ISR( WDT_vect ) {
+    /* dummy */
+    wdt_disable();
+}
